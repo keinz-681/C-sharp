549199d18dc9958f9f24341e9323c7f81f15ac0f
KEINZ <76252197+keinz-681@users.noreply.github.com>
2021-08-13 14:51:50 +0900

    コメント化

--- RollerController.cs
+++ RollerController.cs
@@ -6,17 +6,17 @@ public class RouletteController : MonoBehaviour
 {
     float rotSpeed = 0;
 
-    void Start()
+    void Start()//開始時にi度だけ実行される
     {
         
     }
 
-    void Update()
+    void Update()//すべてのコードが一往復したときに再び実行される
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0))//マウスが下がった時
         {
-            this.rotSpeed = 10;
+            this.rotSpeed = 10;//これの回転角度を10と指定
         }  
-        transform.Rotate(0,0,this.rotSpeed);
+        transform.Rotate(0,0,this.rotSpeed);//1度実行される毎に10°回転
     }
 }
