7f7b39528f9f562127df4258228dca432f05f81a
KEINZ <76252197+keinz-681@users.noreply.github.com>
2021-10-09 12:35:27 +0900

    Update SceneChanger.cs

--- SceneChanger.cs
+++ SceneChanger.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;//シーン切り替えに必要。
 public class Pulic変数 : MonoBehaviour{
-    public string 対象Scene;
+    public string 対象Scene;//インスペクターで切り替えるシーンを入力。
     public void Onclick(){
-        SceneManager.LoadScene(対象Scene);
+        SceneManager.LoadScene(対象Scene);//ボタンをクリックした時にシーンを切り替える。
     }
 }
